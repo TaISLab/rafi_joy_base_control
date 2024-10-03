e8219bec449b582fbbb70eb94cb73793d3443eef
Rodrigo Castro Ochoa <rcastro@uma.es>
2024-10-03 17:05:07 +0200

    Modificar cabeceras
    Modificar cabeceras

--- include/teleop_twist_joy/teleop_twist_joy.h
+++ include/teleop_twist_joy/teleop_twist_joy.h
@@ -1,25 +1,16 @@
-/**
-Software License Agreement (BSD)
+/*
 
-\authors   Mike Purvis <mpurvis@clearpathrobotics.com>
-\copyright Copyright (c) 2014, Clearpath Robotics, Inc., All rights reserved.
+ROS Package: joy_base_control
+File: teleop_twist_joy.h
+Author: Rodrigo Castro Ochoa, rcastro@uma.es, University of Málaga.
 
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that
-the following conditions are met:
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the
-   following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
-   following disclaimer in the documentation and/or other materials provided with the distribution.
- * Neither the name of Clearpath Robotics nor the names of its contributors may be used to endorse or promote
-   products derived from this software without specific prior written permission.
+Description:
+Teleoperation of a omnidirectional mobile plattform.
+This node subscribes to the topic /Joy and publishes a Twist message in the topic /cmd_vel.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WAR-
-RANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, IN-
-DIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
-OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Credits:
+This code is based on the teleop_twist_joy project, available at: http://wiki.ros.org/teleop_twist_joy
+
 */
 
 #ifndef TELEOP_TWIST_JOY_TELEOP_TWIST_JOY_H
